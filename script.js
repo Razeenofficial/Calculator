aebaec4fcb3e64c93a3c2ec5b3e66428494e83b8
Razeenofficial <100760362+Razeenofficial@users.noreply.github.com>
2023-08-29 21:27:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const display = document.getElementById("display");
+    let currentInput = "";
+    let currentOperator = "";
+    let prevInput = "";
+    let result = "";
+
+    function updateDisplay() {
+        display.textContent = currentInput || "0";
+    }
+
+    function clear() {
+        currentInput = "";
+        currentOperator = "";
+        prevInput = "";
+        result = "";
+        updateDisplay();
+    }
+
+    function handleNumberClick(number) {
+        if (currentInput === "0" || currentInput === result) {
+            currentInput = number;
+        } else {
+            currentInput += number;
+        }
+        updateDisplay();
+    }
+
+    function handleOperatorClick(operator) {
+        if (currentInput !== "") {
+            prevInput = currentInput;
+            currentInput = "";
+            currentOperator = operator;
+        }
+    }
+
+    function handleEqualsClick() {
+        if (currentInput !== "" && prevInput !== "") {
+            const num1 = parseFloat(prevInput);
+            const num2 = parseFloat(currentInput);
+            switch (currentOperator) {
+                case "+":
+                    result = (num1 + num2).toString();
+                    break;
+                case "-":
+                    result = (num1 - num2).toString();
+                    break;
+                case "*":
+                    result = (num1 * num2).toString();
+                    break;
+                case "/":
+                    result = (num1 / num2).toString();
+                    break;
+            }
+            currentInput = result;
+            prevInput = "";
+            currentOperator = "";
+            updateDisplay();
+        }
+    }
+
+    function handleBackspaceClick() {
+        if (currentInput.length > 0) {
+            currentInput = currentInput.slice(0, -1);
+            updateDisplay();
+        }
+    }
+
+    function handleDecimalClick() {
+        if (!currentInput.includes(".")) {
+            currentInput += ".";
+            updateDisplay();
+        }
+    }
+
+    // ...
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(button => {
+    button.addEventListener("click", function() {
+        const buttonText = this.textContent;
+        if (buttonText >= "0" && buttonText <= "9") {
+            handleNumberClick(buttonText);
+        } else if (buttonText === "+" || buttonText === "-" || buttonText === "*" || buttonText === "/") {
+            handleOperatorClick(buttonText);
+        } else if (buttonText === "C") {
+            clear();
+        } else if (buttonText === "=") {
+            handleEqualsClick();
+        } else if (buttonText === "←") { // Corrected text content
+            handleBackspaceClick();
+        } else if (buttonText === ".") {
+            handleDecimalClick();
+        }
+    });
+});
+
+});
